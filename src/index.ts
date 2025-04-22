863a3c25fea98f42bea118294ac596c06d9cba2e
Caio Teixeira <caioedut@gmail.com>
2025-04-22 16:36:27 -0300

    fix: add collection to exports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import * as Arr from './helpers/Arr.ts';
+import * as Collection from './helpers/Collection.ts';
 import * as Num from './helpers/Num.ts';
 import * as Obj from './helpers/Obj.ts';
 import * as Str from './helpers/Str.ts';
 import * as Util from './helpers/Util.ts';
 
-export { Arr, Num, Obj, Str, Util };
+export { Arr, Collection, Num, Obj, Str, Util };
